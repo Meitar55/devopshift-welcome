771e43c3cd494dc633a1c44a9489a1fa24d99445
Your Name <you@example.com>
2025-02-05 14:33:33 +0000

    created infrastructure with open ports 22 and 80 installs apache and configurations

---
+++ labs/lab 103/global.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+ region = var.region
+}
+
+variable "region" {
+ default = "us-east-1"
+}
+
+
+#variable "ami" {
+ #default = var.ami_id
+ #}
+variable "vm_name" {
+ default = "vm-Meitar"
+}
+
+variable "admin_username" {
+ default = "admin-user"
+}
+
+variable "admin_password" {
+ default = "Password123!"
+}
+
+variable "vm_size" {
+ default = "t2.micro"
+}
+
+data "aws_ami" "my-privateami" {
+    owners = ["self"]  # Queries only AMIs owned by your account
+
+}
+
+data "aws_ami" "vm_ami"{
+ owners = ["self"]
+ filter {
+   name = "name"
+   values= ["terraform-workshop-image-do-not-delete"]
+ }
+}
+
+
